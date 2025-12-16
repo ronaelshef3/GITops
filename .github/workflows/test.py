ef3766b24c839bb87a2f24c5378f981529f217b1
Aharon <you@example.com>
2025-12-16 16:50:52 +0200

    add test

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -8,6 +8,7 @@ class MyTestCase(unittest.TestCase):
     def test_something1(self):
         self.assertEqual(True, True)  # add assertion here
 
-
+    def test_something2(self):
+        self.assertEqual(True, True)  # add assertion here
 if __name__ == '__main__':
     unittest.main()
